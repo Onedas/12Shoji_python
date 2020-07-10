d93db4a997bb6e98af0907deb024571dada8ab62
Onedas <wlstjd940317@gmail.com>
2020-07-11 04:03:31 +0900

    add Captive rule

--- 12shoji/game.py
+++ 12shoji/game.py
@@ -1,10 +1,11 @@
 
 class Piece(object):
 
-	def __init__(self,color,position,name):
-		self.name = name
+	def __init__(self,color,position,name,captive=False):
+		self.name = "%-4s"%name
 		self.position = position
 		self.Color = color
+		self.Captive = captive
 
 	def isValid(self,startpos,endpos,Color,gameboard):
 		if endpos in self.availableMoves(startpos[0],startpos[1],gameboard, Color=Color):
@@ -22,19 +23,27 @@ class Piece(object):
 
 	def _availableMoves(self,gameboard,Color,dMoves):
 		answer = []
-		x = self.position[0]
-		y = self.position[1]
-		for dx,dy in dMoves:
-			xtemp, ytemp = x+dx, y+dy
-			if self.isInBounds(xtemp,ytemp):
-				target = gameboard.get((xtemp,ytemp))
-				if target == None:
-					answer.append((xtemp,ytemp))
-				elif target.Color != Color:
-					answer.append((xtemp,ytemp))
+		if not self.Captive:
+			x = self.position[0]
+			y = self.position[1]
+			for dx,dy in dMoves:
+				xtemp, ytemp = x+dx, y+dy
+				if self.isInBounds(xtemp,ytemp):
+					target = gameboard.get((xtemp,ytemp))
+					if target == None:
+						answer.append((xtemp,ytemp))
+					elif target.Color != Color:
+						answer.append((xtemp,ytemp))
+		elif self.Captive:
+			if self.Color =="RED":
+				xtemp = 0
+			elif self.Color =="GREEN":
+				xtemp = 3
 
+			for ytemp in range(0,3):
+				if not gameboard.get((xtemp,ytemp)):
+					answer.append((xtemp,ytemp))
 		return answer
-
 
 	def isInBounds(self,x,y):
 		"Checks if a position is on the board"
@@ -91,6 +100,10 @@ class Hoo(Piece):
 			dMoves = [(-1,-1),(-1,0),(-1,1),(0,-1),(0,-1),(1,0)]
 		return self._availableMoves(gameboard,Color,dMoves)
 
+"""
+	
+"""
+
 class jangi(object):
 
 	def __init__(self):
@@ -121,7 +134,11 @@ class jangi(object):
 		self.gameboard[(3,2)] = Sang("GREEN",(3,2),"Sang")
 		self.gameboard[(2,1)] = Ja("GREEN",(2,1),"Ja")
 
-	def printBoard(self):
+	def _printGame(self):
+		self.__printBoard()
+		self.__printCaptive()
+
+	def __printBoard(self):
 		print()
 		print("====",self.turnName[self.Turn%2]+'Turn ====')
 		print()
@@ -134,55 +151,107 @@ class jangi(object):
 				print("%-5s"%str(item)+"|", end=" ")
 			print()
 			print("  "+"ㅡ"*11)
-		print("RED Captive :",self.REDCaptive)
-		print("GRE Captive :",self.GREENCaptive)
+
+	def __printCaptive(self):
+		print(" y|  4   |  5   |  6   |  7   |  8   |")
+		print("x ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ")
+		print("4",self.REDCaptive, "REDCaptive")
+		print("5",self.GREENCaptive,"GREENCaptive")
 
 	def iscanSelect(self,x,y):
-		target = self.gameboard.get((x,y))
-		if target:
-			# print(target.Color,target.name,end= ' : ')
-			if target.Color == self.turnName[self.Turn%2]:
-				return True
-		return False
+		if x in (0,1,2,3) and y in (0,1,2):
+			target = self.gameboard.get((x,y))
+			if target:
+				# print(target.Color,target.name,end= ' : ')
+				if target.Color == self.turnName[self.Turn%2]:
+					return True
+			return False
+
+		elif x == 4 and y in (4,5,6,7,8):
+
+			if self.Turn%2 == 0: # right turn
+				if y-4 < len(self.REDCaptive): # have captive
+					return True
+			return False
+
+		elif x == 5 and y in (4,5,6,7,8):
+			if self.Turn%2 == 1:
+				if y-4 < len(self.GREENCaptive):
+					return True
+			return False
+
+		else:
+			return False
 
 	def iscanMove(self,x1,y1,x2,y2):
 		if self.iscanSelect(x1,y1):
-			target = self.gameboard.get((x1,y1))
+			if x1 in (0,1,2,3) and y1 in (0,1,2):
+				target = self.gameboard.get((x1,y1))
+			elif x1 == 4 and y1 in (4,5,6,7,8):
+				target = self.REDCaptive[y1-4]
+			elif x1 == 5 and y1 in (4,5,6,7,8):
+				target = self.GREENCaptive[y1-4]
+			print(target.availableMoves(self.gameboard))
 			if (x2,y2) in target.availableMoves(self.gameboard):
 				return True
 		return False
 
 	def Move(self,x1,y1,x2,y2):
 		if self.iscanMove(x1,y1,x2,y2):
-			target1 = self.gameboard.get((x1,y1))
+			if x1 in (0,1,2,3) and y1 in (0,1,2):
+				target1 = self.gameboard.get((x1,y1))	
+				del self.gameboard[(x1,y1)]
+
+			elif x1 == 4 and y1 in (4,5,6,7,8):
+				target1 = self.REDCaptive.pop(y1-4)
+				target1.Captive = False
+
+			elif x1 == 5 and y1 in (4,5,6,7,8):
+				target1 = self.GREENCaptive.pop(y1-4)
+				target1.Captive = False
+
 			target2 = self.gameboard.get((x2,y2))
 			if target2: #Color = self.playerTurn, #Name = target2.name
-				eval("self.{Color}Captive.append({Name}('{Color}',None,'{Name}'))".format(Color=self.turnName[self.Turn%2],Name=target2.name))
+				Color = self.turnName[self.Turn%2]
+				Name = "Ja" if target2.name == "Hoo " else target2.name #Hoo가 죽으면 Ja로 바꿔줌
+				eval("self.{Color}Captive.append({Name}('{Color}',None,'{Name}',True))".format(Color=Color,Name=Name))
+
 			target1.position = (x2,y2)
+			# Ja가 상대편 진영에 가면 Hoo로 변환
+			print(target1.name, target1.name =="Ja  ")
+			if target1.name == "Ja  ":
+				if target1.Color == "RED" and target1.position[0] == 3:
+					target1 = Hoo("RED",(x2,y2),"Hoo")
+				elif target1.Color == "GREEN" and target1.position[0] ==0:
+					target1 = Hoo("GREEN",(x2,y2),"Hoo")
+
 			self.gameboard[(x2,y2)] = target1
-			del self.gameboard[(x1,y1)]	
 			self.Turn +=1
+
 			return True
 
 	def main(self):
+		"""
+			CLI 로 실행
+		"""
+
 		running = True
 		while running:
-			prisoner_state = False
-			Turn_end = False
+
 			#select piece
-			self.printBoard()
-			x1,y1 = map(int,input('select piece (x y) : ').split())
+			self._printGame()
+			
+			try:x1,y1 = map(int,input('select piece (x y) : ').split())
+			except:continue
 
-			if prisoner_state:
-				pass
-			else: # Piece select
-				if self.iscanSelect(x1,y1):
-					x2,y2 = map(int,input('select move position (x y) : ').split())
-				else: continue
+			if self.iscanSelect(x1,y1):
+				try:x2,y2 = map(int,input('select move position (x y) : ').split())
+				except:continue
+			else: continue
 
-				if self.iscanMove(x1,y1,x2,y2):
-					self.Move(x1,y1,x2,y2)
-				else: continue
+			if self.iscanMove(x1,y1,x2,y2):
+				self.Move(x1,y1,x2,y2)
+			else: continue
 
 if __name__ == "__main__":
 	game = jangi()
